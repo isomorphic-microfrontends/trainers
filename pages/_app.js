db3f69a340352739c26c67be12e2fb2456f7268a
Joel Denning <joeldenning@gmail.com>
2021-05-27 12:06:17 -0600

    Fix unmounting

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,22 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
 
-function MyApp({ Component, pageProps }) {
+let numMounts = 0;
+
+function MyApp({ Component, pageProps, router }) {
+  useEffect(() => {
+    if (numMounts++ > 0) {
+      // This is hacky and hopefully a better implementation will be found.
+      // NextJS remounting doesn't recreate the router singleton, so we
+      // have to readd its popstate listener every time except the first time.
+      window.addEventListener('popstate', router.onPopState)
+    }
+    return () => {
+      // NextJS doesn't clean up its popstate listener manually, so we do it for it
+      window.removeEventListener('popstate', router.onPopState)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
--- single-spa/single-spa-next.js
+++ single-spa/single-spa-next.js
@@ -3,7 +3,7 @@ import initNext from 'next/dist/client/index.js'
 import { unmountComponentAtNode } from 'react-dom'
 
 export default function singleSpaNext(opts) {
-  let renderCtx, render
+  let renderCtx, render, originalNextEl = document.querySelector(`#single-spa-application\\:\\@isomorphic-mf\\/trainers #__next`)
 
   return {
     async bootstrap() {
@@ -15,10 +15,15 @@ export default function singleSpaNext(opts) {
       await new Promise((resolve) => {
         displayContent(resolve)
       })
+      let applicationContainer = document.getElementById('single-spa-application:@isomorphic-mf/trainers')
+      let nextEl = applicationContainer.querySelector('#__next')
+      if (!nextEl) {
+        applicationContainer.appendChild(originalNextEl)
+      }
       await render(renderCtx)
     },
     async unmount(props) {
-      const unmounted = unmountComponentAtNode(document.getElementById('__next'))
+      const unmounted = unmountComponentAtNode(originalNextEl)
       if (!unmounted) {
         throw Error(`single-spa-next: unmountComponentAtNode() returned false, indicating the application failed to unmount`)
       }
